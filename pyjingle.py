8134e0ee5f8e68e6ca3d72ba946c3219505922b1
Joe Stein <jas14@cs.brown.edu>
2016-11-13 16:38:05 -0500

    added suggested jingles + jingle dir (path-ish)

--- pyjingle.py
+++ pyjingle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import argparse
+import os
+import sys
 import re
 from notes import Note
 # from subprocess import call
@@ -23,28 +25,45 @@ def main():
         player.set_shift(args.shift)
 
     line_no = 0
-    with open(args.jingle) as jingle_file:
-        ws = re.compile(r'[ \t]+')
-        for line in jingle_file.xreadlines():
-            line_no += 1
-            line = line.strip()
-            if line == '' or line[0] == '%':
-                # ignore blank lines and comments
-                continue
-            elif line[0] == '*':
-                # setting line
-                key, val = line[1:].strip().split(' ')
-                if key == 'speed':
-                    # use suggested speed
-                    player.set_speed(int(val))
-                else:
-                    print ("Parse error: Unrecognized setting \"{0}\" "
-                           "on line {1}".format(key, line_no))
-                    return
-                continue
+    jingle_file = None
+    try:
+        jingle_file = open(args.jingle, 'r')
+    except:
+        pass
+    if jingle_file is None:
+        my_dir = os.path.dirname(os.path.realpath(__file__))
+        jingle_dir = os.path.join(my_dir, 'jingles')
+        try:
+            jingle_file = open(os.path.join(jingle_dir, args.jingle))
+        except:
+            print("Couldn't open jingle file.")
+            if os.path.isdir(jingle_dir):
+                print("Suggested jingles:")
+                for jingle in os.listdir(jingle_dir):
+                        print("\t{0}".format(jingle))
+            sys.exit(1)
 
-            for note in ws.split(line):
-                player.play(note)
+    ws = re.compile(r'[ \t]+')
+    for line in jingle_file.xreadlines():
+        line_no += 1
+        line = line.strip()
+        if line == '' or line[0] == '%':
+            # ignore blank lines and comments
+            continue
+        elif line[0] == '*':
+            # setting line
+            key, val = line[1:].strip().split(' ')
+            if key == 'speed':
+                # use suggested speed
+                player.set_speed(int(val))
+            else:
+                print ("Parse error: Unrecognized setting \"{0}\" "
+                       "on line {1}".format(key, line_no))
+                return
+            continue
+
+        for note in ws.split(line):
+            player.play(note)
 
 if __name__ == "__main__":
     main()
